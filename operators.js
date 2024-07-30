ee308a8b9118e38cb8514834d48d221ed453f2d2
Berkcan <ucarberkcan83@gmail.com>
2024-07-30 12:41:06 +0300

    fourth commit - file opperatorts.js

---
+++ operators.js
@@ -0,0 +1,26 @@
+let a = true;
+let b = true;
+let c = false;
+let d = true;
+let e = true;
+
+// !
+// if (!b) {
+//   console.log("ok");
+// } else {
+//   console.log("Not ok");
+// }
+
+// &&
+// if (a && b) {
+//   console.log("ok");
+// } else {
+//   console.log("Not ok");
+// }
+
+// ||
+if (a || b || c || d || e) {
+  console.log("ok");
+} else {
+  console.log("Not ok");
+}
