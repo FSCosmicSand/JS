dd0c0248dad788b84b1edad553298bff4e42b2c8
Berkcan <ucarberkcan83@gmail.com>
2024-08-09 11:38:51 +0300

    (8) Commit - find min,max value in object with method map or forEach

--- objectValueAndobjectKey.js
+++ objectValueAndobjectKey.js
@@ -6,21 +6,57 @@ const berkHobbyRating = {
   pingpong3: 250,
   pingpong4: 1500,
 };
-
+// ______________ Max /map (forEach)/ ____________________________
 function biggestRating(obj) {
   const arrayValueHobbys = Object.values(obj);
-  console.log(Object.values(obj));
+  // console.log(Object.values(obj));
+
   let maxRating = 0;
 
-  for (let i = 0; i < Object.values(obj).length; i++) {
-    if (maxRating < arrayValueHobbys[i]) {
-      maxRating = arrayValueHobbys[i];
+  arrayValueHobbys.forEach((arrayValueHobby) => {
+    {
+      if (maxRating < arrayValueHobby) {
+        maxRating = arrayValueHobby;
+      }
     }
-  }
+    return maxRating;
+  });
+
   return maxRating;
 }
+// ______________ Min /map (forEach)/ ____________________________
+function minimumRating(obj) {
+  const arrayValueHobbys = Object.values(obj);
+  // console.log(Object.values(obj));
 
-console.log(biggestRating(berkHobbyRating));
+  let minRating = 10000;
+
+  arrayValueHobbys.forEach((arrayValueHobby) => {
+    {
+      if (minRating > arrayValueHobby) {
+        minRating = arrayValueHobby;
+      }
+    }
+    return minRating;
+  });
+
+  return minRating;
+}
+// function biggestRating(obj) {
+//   const arrayValueHobbys = Object.values(obj);
+//   console.log(Object.values(obj));
+//   let maxRating = 0;
+
+//   for (let i = 0; i < Object.values(obj).length; i++) {
+//     if (maxRating < arrayValueHobbys[i]) {
+//       maxRating = arrayValueHobbys[i];
+//     }
+//   }
+//   return maxRating;
+// }
+
+console.log("maximum: ", biggestRating(berkHobbyRating));
+console.log("minimum: ", minimumRating(berkHobbyRating));
 
 // let j = 0;
 // for (let i = 0; i < 10; i++) {
