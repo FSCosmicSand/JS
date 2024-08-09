5087107d1bdb9daa76786c050c3ba725c3428903
Berkcan <ucarberkcan83@gmail.com>
2024-08-09 12:28:36 +0300

    (11)Commit - if method

---
+++ if_method.js
@@ -0,0 +1,21 @@
+const a = 5;
+
+// if (a === 10 || a < 4 || a === 5) {
+//   console.log("a satisfied condition");
+// } else {
+//   console.log("a not satisfied condition");
+// }
+
+if (a === 10) {
+  console.log("a is not satisfied condition");
+} else if (a < 4) {
+  console.log("a is not satisfied condition");
+} else if (a === 5) {
+  console.log("a is satisfied condition");
+}
+
+// if (a < 10 && a > 4) {
+//   console.log("a satisfied condition");
+// } else {
+//   console.log("a not satisfied condition");
+// }
